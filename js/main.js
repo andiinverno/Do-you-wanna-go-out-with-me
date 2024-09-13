4e1ff0b3ebe552eb20ee2e1ebbc750fef11db2a1
andiinverno <116290548+andiinverno@users.noreply.github.com>
2024-09-13 16:41:36 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('thx,udah milih yes').split('')
+    const titles = ('for you').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
